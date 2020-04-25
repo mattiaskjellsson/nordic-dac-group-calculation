94b84f60a01a77e1bd0cc382e8f9096672bdf0ab
Mattias <u.m.kjellsson@gmail.com>
2020-04-25 15:49:44 +0700

    Prep for heruko

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.use(function(req, res, next) {
-  if (req.headers[`x-forwarded-proto`] === `https`) {
-    res.redirect(`http://${req.hostname}${req.url}`);
-  } else {
-    next();
-  }
-});
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
